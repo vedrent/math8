d3d46e8b93816e27944f36d1a4a24e047ddcd349
vedro <vedrentoff@gmail.com>
2024-11-08 17:41:09 +0300

    added tp and new maps

--- Mainform.cs
+++ Mainform.cs
@@ -6,6 +6,46 @@ namespace math8
 {
     public partial class MainForm : Form
     {
+        private int currentMazeIndex = 0;
+        private int[,,] mazes = new int[3, 10, 10]
+        {
+            { // maze0
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 0, 1, 0, 1, 1, 0, 1 },
+                { 1, 0, 0, 0, 1, 0, 0, 1, 0, 1 },
+                { 1, 0, 1, 0, 1, 1, 1, 1, 0, 1 },
+                { 1, 0, 1, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 1, 1, 1, 1, 1, 0, 1, 1 },
+                { 1, 0, 1, 0, 0, 0, 1, 1, 1, 1 },
+                { 1, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 }
+            },
+            { // maze1 (создаваемый вручную)
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+            },
+            { // maze2
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 0, 0, 0, 1, 1, 1, 0, 0, 0, 1 },
+                { 1, 1, 0, 0, 1, 0, 1, 1, 0, 1 },
+                { 1, 0, 0, 0, 1, 0, 0, 1, 0, 1 },
+                { 1, 0, 1, 0, 0, 1, 1, 1, 0, 1 },
+                { 1, 0, 1, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 1, 1, 1, 1, 1, 0, 1, 1 },
+                { 1, 0, 1, 0, 0, 0, 1, 1, 1, 1 },
+                { 1, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 }
+            }
+        };
         private int[,] maze1 = new int[10, 10];
         //private int[,] maze1 = {
         //                      { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -34,9 +74,14 @@ namespace math8
                               };
 
         private int playerX = 0, playerY = 1;
-        private bool editMode = true;
-        //private Bitmap pathMap;
-        //private PathForm pathForm;
+        private bool editMode = false;
+
+        private (int x, int y, int Maze, int targetX, int targetY, int targetMaze)[] teleporters = {
+            (8, 9, 0, 1, 1, 1),  // Телепорт из maze0 в maze1
+            (8, 8, 1, 0, 1, 2),  // Телепорт из maze1 в maze2
+            (6, 1, 2, 0, 1, 0)   // Телепорт из maze2 в maze0
+        };
+
         public MainForm()
         {
             //InitializeComponent();
@@ -76,14 +121,14 @@ namespace math8
                 case Keys.Left: newX--; break;
                 case Keys.Right: newX++; break;
                 case Keys.Enter:
-                    if (editMode && maze1[playerY, playerX] == 0)
-                        maze1[playerY, playerX] = 1;
+                    if (editMode && mazes[currentMazeIndex, playerY, playerX] == 0)
+                        mazes[currentMazeIndex, playerY, playerX] = 1;
                     break;
             }
-            if (newX >= 0 && newX < maze0.GetLength(1) && newY >= 0 && newY <
-           maze0.GetLength(0))
+            if (newX >= 0 && newX < mazes.GetLength(2) && newY >= 0 && newY <
+           mazes.GetLength(1))
             {
-                if (editMode || (!editMode && maze0[newY, newX] == 0)) {
+                if (editMode || (!editMode && mazes[currentMazeIndex, newY, newX] == 0)) {
                     playerX = newX;
                     playerY = newY;
                 }
@@ -94,19 +139,33 @@ namespace math8
                 
                 //pathForm.Invalidate();
             }
+            CheckTeleport();
             Invalidate();
+        }
+        private void CheckTeleport()
+        {
+            foreach (var teleporter in teleporters)
+            {
+                if (teleporter.x == playerX && teleporter.y == playerY && currentMazeIndex == teleporter.Maze)
+                {
+                    playerX = teleporter.targetX;
+                    playerY = teleporter.targetY;
+                    currentMazeIndex = teleporter.targetMaze;
+                    break;
+                }
+            }
         }
         private void OnPaint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
             int cellSize = 40;
-            var maze = editMode ? maze1 : maze0;
+            //var maze = editMode ? maze1 : maze0;
 
-            for (int y = 0; y < maze.GetLength(0); y++)
+            for (int y = 0; y < mazes.GetLength(1); y++)
             {
-                for (int x = 0; x < maze.GetLength(1); x++)
+                for (int x = 0; x < mazes.GetLength(2); x++)
                 {
-                    if (maze[y, x] == 1)
+                    if (mazes[currentMazeIndex, y, x] == 1)
                     {
                         g.FillRectangle(Brushes.Black, x * cellSize, y * cellSize,
                        cellSize, cellSize);
@@ -121,10 +180,10 @@ namespace math8
                         g.FillRectangle(Brushes.Blue, x * cellSize, y * cellSize,
                        cellSize, cellSize);
                     }
-                    if (!editMode && x == 8 && y == 9)
+                    foreach (var teleporter in teleporters)
                     {
-                        g.FillRectangle(Brushes.Green, x * cellSize, y * cellSize,
-                       cellSize, cellSize);
+                        if (teleporter.x == x && teleporter.y == y && currentMazeIndex == teleporter.Maze)
+                            g.FillRectangle(Brushes.Purple, x * cellSize, y * cellSize, cellSize, cellSize);
                     }
                 }
             }
